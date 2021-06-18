f55bbb760049a41690a5e6574a280a07306465fb
pvvishnu557 <71384832+pvvishnu557@users.noreply.github.com>
2021-06-18 19:07:06 +0530

    Create simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,67 @@
+#simple calculator
+	
+
+>>> def add (num1, num2):
+	result num1+num2
+	
+
+	
+>>> def sub(num1,num2):
+	num1-num2
+
+	
+>>> def multi(num1,num2)
+SyntaxError: invalid syntax
+>>> def multi(num1,num2):
+	num1*num2
+
+	
+>>> def div(num1,num2):
+	num1/num2
+
+	
+>>> num1=float(input("enter the first number:"))
+enter the first number:43
+>>> num2=float(input("enter the second number:"))
+enter the second number:23.54
+>>> print("what you want\n"
+           +"1.addition\n"
+           +"2.subtration\n"
+           +"3.multiplication\n"
+           +"4.division")
+what you want
+1.addition
+2.subtration
+3.multiplication
+4.division
+>>> select=int(input("enter the number 1,2,3,4"))
+enter the number 1,2,3,42
+>>> select=int(input("enter the number 1,2,3, or 4:"))
+enter the number 1,2,3, or 4:2
+>>> if select==1:
+	print(num1+num2)
+elif select==2:
+	print(num1-num2)
+elif select==3:
+	print(num1*num2)
+elif select==4:
+	print(num1/num2)
+else:
+	print(select a valid number)
+	
+SyntaxError: invalid syntax
+>>> if select==1:
+	print(num1+num2)
+elif select==2:
+	print(num1-num2)
+elif select==3:
+	print(num1*num2)
+elif select==4:
+	print(num1/num2)
+else:
+	print("select a valid number")
+
+	
+19.46
+>>> print("Than you for using leo's calculator")
+Than you for using leo's calculator
